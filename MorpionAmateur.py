5c9b45ee8ec885dc6c12a842a77e2a32bcbad05d
TheM00NCAKE <isabelleye2110@gmail.com>
2025-07-29 23:22:16 +0200

    Update MorpionAmateur.py

--- MorpionAmateur.py
+++ MorpionAmateur.py
@@ -2,53 +2,27 @@ import tkinter as tk
 import math
 import time
 #les states servent à communiquer les différentes phases au cours du jeu
-state=0      #state 0 : le jeu ne se lance pas
+state=0     #state 0 : le jeu ne se lance pas
 compteur=1   #nombre de tour par défaut, c'est à dire 1. Le maximum est 9
 scoreO=0
 scoreX=0
+#Ma fenêtre qui va servir à tout afficher
+window=tk.Tk()                            
+window.geometry("900x800")
 def jeu():
     global state, compteur
-    if state!=1 :       #signifie que le jeu a été activé pour la première fois
-        introduction()
-    if state==1:        #state 1 : le jeu peut commencer si le joueur a choisi de vouloir jouer
+    if state==1 or state==0:        #state 1 : le jeu peut commencer si le joueur a choisi de vouloir jouer
         plateau()
-    return state
-     
-def introduction():
-    global state
-    intro=input("Bonjour, ceci est un jeu de morpion standard qui n'est pas censé voir le jour, ceci est simplement un exercice pour remettre à jour les vieilles connaissances (et aussi parce que je m'ennuie mdr), peut importe... Veux tu commencer une partie ? \nOui : 1\nNon : 2 \nréponse : ")
-    if intro=="2":
-        print("Très bien, au revoir !")
-        state=0    
-    elif intro!="2" and intro !="1":
-        a=input("Il n'y a que 2 options, comment as-tu fais pour te tromper ? Aller, réassaye encore...  \nOui : 1 \nNon : 2 \nréponse : ")
-        if a!="1" and a!="2":
-            print ("Puisque c'est comme ça, au revoir.")
-        elif a=="2":
-            print("Très bien, à bientôt j'espère.")
-        else:
-            introduction()     #redonne une chance au joueur de refaire un choix de début
     else:
-        print("D'accord ! Une fenêtre va apparaitre sur la barre des tâches.")
-        state=1                  #state=1 signifie que le joueur veut jouer
-    return state
-        
-def replay():
-    global state
-    s=input("veux tu rejouer ? \nOui : 1\nNon : n'importe quel autre touche\nréponse : ")
-    if s=="1" :
-        state=1
-        jeu()
-    else:
-        print("Ok, j'espère que cela vous a plu, au revoir!")
+        raise Exception("what ??????")
     return state
 
 def plateau():
+    global state, compteur
     board_=[[0,0,0],[0,0,0],[0,0,0]]    #au début de partie, le tableau est encore vide, donc les cases n'ont aucune valeur
     def cond_victoire(joueur):
         global state,compteur,scoreO,scoreX
         if (any(all(board_[i][j] == joueur for j in range(3)) for i in range(3)) or any(all(board_[i][j] == joueur for i in range(3)) for j in range(3)) or all(board_[i][i] == joueur for i in range(3)) or all(board_[i][2 - i] == joueur for i in range(3))):  
-            etat.itemconfig(texte,text=f"   Victoire du joueur {'O' if joueur==1 else 'X'}! \nLa fenêtre va se fermer dans 3s", font=("Arial",30))
             compteur=0      #le compteur de cases rempli se remet à 0 car la partie est terminée (se prépare en cas de nouvelle partie)
             if joueur==1:
                 state="O_win"  
@@ -79,28 +53,66 @@ def plateau():
             else:
                 compteur-=1             #si le mec clique n'importe comment sur une case déjà cliqué, ça passe pas son tour yk ?
             if compteur==9 and state==1:
-                etat.itemconfig(texte,text="Match nul, dommage... \nLa fenêtre va se fermer dans 3s", font=("Arial",30))
                 state="Match_nul"
             compteur+=1
-            if state!=1:
-                window.after(3000, lambda: [window.destroy(), replay()])
+            if state!=1: #state ne peut pas être à 0, donc si state!=1, la partie est terminée (match_nul, O_win ou X_win)
+                TheBoard.destroy()
+                zone_end=tk.Frame(window,width=600,height=400,bg="purple")
+                #highlightbackground c'est pour les marges entre les canvas qui sont blanches :)
+                test=tk.Canvas(zone_end,width=600,height=200,bg="purple",highlightbackground="purple")
+                message=('Match nul, dommage...' if state=='Match_nul' else f'Victoire du joueur {"O" if state=="O_win" else "X"}')
+                test.create_text(300,100, text=message, font=("Arial",30),justify="center")
+                bstart=tk.Canvas(zone_end,width=200, height=50,bg="green", highlightbackground="purple")
+                bstart.bind("<Button-1>",lambda event:[zone_end.destroy(),jeu()])
+                bstart.create_text(100,25, text="Replay",font=("Arial", 20))
+                bend=tk.Canvas(zone_end,width=200,height=50,bg="red", highlightbackground="purple")
+                bend.create_text(100,25,text="Quit",justify="center",font=("Arial",20))
+                bend.bind("<Button-1>",lambda event: window.destroy())
+                test.pack()
+                bstart.pack(side=tk.BOTTOM)
+                bend.pack(side=tk.BOTTOM)
+                zone_end.pack(pady=200)
+                #On revient sur la state de début de partie, mais les scores sont pris en compte
+                state=1
             window.mainloop()
-            
-    window=tk.Tk()                            #mon plateau morpion et mon canvas avec les grilles
-    window.geometry("900x800")
-    etat=tk.Canvas(window,width=900,height=100,bg="dim gray")
-    score=tk.Canvas(window,width=900,height=60,bg="light gray")
-    etat.pack(side=tk.TOP)
-    score.pack()
-    texte=etat.create_text(450,50,text="Le morpion standard",font=("Arial", 30), justify="center")
+
+    def PackBoard(x, event=None):
+        global state
+        if x==1:
+            zone_debut.destroy()
+        state=1
+        etat.pack(side=tk.TOP)
+        score.pack()
+        board.bind("<Button-1>", action)
+        board.pack(side=tk.BOTTOM,pady=10)
+        TheBoard.pack()
+    zone_debut=tk.Frame(window,width=900,height=800)
+    debut=tk.Canvas(zone_debut,width=900,height=500, bg='light grey')
+    debut.create_text(450, 250,text="Bienvenue sur ce jeu de con", font=("Arial",30),justify="center")
+    #esthétique du plateau après avoir lancé Start boutton
+    TheBoard=tk.Frame(window,width=900,height=600)
+    etat=tk.Canvas(TheBoard,width=900,height=100,bg="dim gray")
+    score=tk.Canvas(TheBoard,width=900,height=60,bg="light gray")
+    etat.create_text(450,50,text="Le morpion standard",font=("Arial", 30), justify="center",fill="white")
     TexteScore=score.create_text(450,30,text=f"Score du joueur O : {scoreO}      Score du joueur X : {scoreX}",font=("Arial", 15))
-    board=tk.Canvas(window,width=900,height=600,bg="grey")
+    board=tk.Canvas(TheBoard,width=900,height=600,bg="grey")
     board.create_line(0,200,900,200,fill="black")
     board.create_line(0,400,900,400,fill="black")
     board.create_line(300,0,300,900,fill="black")
     board.create_line(600,0,600,900,fill="black")
-    board.bind("<Button-1>", action)
-    board.pack(side=tk.BOTTOM,pady=10)
     window.config(cursor="circle") #le curseur a une forme de cercle
-    window.mainloop()
+    bstart=tk.Canvas(zone_debut,width=200, height=50,bg="green")
+    bstart.bind("<Button-1>",lambda event: PackBoard(1))
+    bstart.create_text(100,25, text="Start",font=("Arial", 20))
+    bend=tk.Canvas(zone_debut,width=200,height=50,bg="red")
+    bend.create_text(100,25,text="Quit",justify="center",font=("Arial",20))
+    bend.bind("<Button-1>",lambda event: window.destroy())
+    if state==0:
+        debut.pack(pady=50)
+        bstart.pack(side=tk.BOTTOM)
+        bend.pack(side=tk.BOTTOM)
+        zone_debut.pack()
+        window.mainloop()
+    else:
+        PackBoard(0)
 jeu()
